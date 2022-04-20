23a62da8105f9adc9276e4392340a90375f9bf41
Spencer Chang <spencerc99@gmail.com>
2022-04-20 23:38:44 +0200

    fix config to be relative

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+    },
+  },
+  base: "./",
+});
